a51e642ef2d6b30c4ad17adb38275b6a4586ade6
khedau <khedau@zscaler.com>
2024-02-21 23:20:12 +0530

    adding react-router-dom and updated script for dev

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 const rootDiv = document.getElementById("root") as HTMLElement;
-
 const root = ReactDOM.createRoot(rootDiv);
 
-root.render(<>Bun React App</>);
+root.render(<RouterProvider router={router} />);
